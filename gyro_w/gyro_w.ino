57faf63af5c15dc196892fca998bc030dd6c7193
Braian Tenorio <tenoriovidalbraian@gmail.com>
2026-05-14 03:15:10 -0300

    working without tasks

--- gyro_w/gyro_w.ino
+++ gyro_w/gyro_w.ino
@@ -9,7 +9,6 @@ float ACCEL_THRESHOLD  = 1.5;
 
 float SMOOTHING_ALPHA  = 0.2;
 float GYRO_DEADZONE    = 0.08;
-int CLICK_FREEZE_MS    = 150;
 
 bool VERTICAL_MODE = false;
 bool INVERT_X      = true;
@@ -34,7 +33,7 @@ float mapFloat(float x, float in_min, float in_max, float out_min, float out_max
 
 void calibrateGyro() {
   float sumX = 0, sumY = 0, sumZ = 0;
-  int samples = 200;
+  int samples = 500;
 
   for (int i = 0; i < samples; i++) {
     sensors_event_t a, g, temp;
@@ -57,8 +56,6 @@ void setup() {
   Serial.begin(115200);
   Wire.begin(21,22);
 
-  pinMode(LEFT_BTN, INPUT_PULLUP);
-  pinMode(RIGHT_BTN, INPUT_PULLUP);
 
   if (!mpu.begin()) {
     Serial.println("MPU6050 not found!");
